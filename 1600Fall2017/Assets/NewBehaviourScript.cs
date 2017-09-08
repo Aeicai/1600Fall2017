a370b2c18cf4181f08ee7d8df3e3a4df27fb72a5
Unknown <aricainicolosi@gmail.com>
2017-09-08 10:43:51 -0600

    added NewBehaviourScripts.cs

---
+++ 1600Fall2017/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    string myString = "Hello";
+
+	void Start () {
+        print(myString);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
