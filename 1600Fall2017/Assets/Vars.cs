39befc55b3eed4079672cc8ac1981f358361c0fc
Unknown <aricainicolosi@gmail.com>
2017-09-08 11:51:57 -0600

    removed classes
    and added other assets for player and powerups

---
+++ 1600Fall2017/Assets/Vars.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Vars : MonoBehaviour {
+
+    public float health = 100;
+
+	// Use this for initialization
+	void Start () {
+        health = 200;
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        health += 20;
+ 
+	}
+}
