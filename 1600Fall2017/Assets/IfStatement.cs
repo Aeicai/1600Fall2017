d0761e73e4d3030f5120ce298efd62191b02d14e
Unknown <aricainicolosi@gmail.com>
2017-09-13 11:45:35 -0600

    Made IfStatements and MatOperators

---
+++ 1600Fall2017/Assets/IfStatement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IfStatement : MonoBehaviour {
+
+
+    public Text input;
+    public string password = "G@m3Pl@y";
+    public bool canPlayGame;
+	
+	
+	void Update () {
+        if (input.text == password)
+        {
+            print("You know the password.");
+            canPlayGame = true;
+        } else {
+            print("The password is incorrect.");
+        }
+        if (canPlayGame)
+        {
+            print("Playing Game");
+        }   else {
+            print("Can't Play Yet, Enter a Correct Password.");
+        }
+	}
+}
