a3f2971ea05b968a48925f9e9f4b4eb36779411c
Unknown <aricainicolosi@gmail.com>
2017-09-27 11:49:54 -0600

    created lists

---
+++ 1600Fall2017/Assets/Lists.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Lists : MonoBehaviour {
+
+	public Lists<GameObject> myWeapon;
+	
+	
+	void OnTriggerEnter(Collider _weapon)
+    {
+        myWeapons.Add(_weapon.gameObject);	
+	}
+}
