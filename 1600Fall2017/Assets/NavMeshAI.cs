7468057cc91ae0818d191d2b49a1f4a673f7661f
Unknown <aricainicolosi@gmail.com>
2017-10-18 11:48:39 -0600

    made changes and made character controls

---
+++ 1600Fall2017/Assets/NavMeshAI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using 
+
+public class NavMeshAI : MonoBehaviour {
+
+    public Transform player;
+    public NavMeshAgent agent;
+		
+	}
+}
