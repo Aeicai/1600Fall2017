6068d1675807bf53adeea36ddf33ff0f64dc75bb
Unknown <aricainicolosi@gmail.com>
2017-10-25 11:55:17 -0600

    made changes to stuff

---
+++ 1600Fall2017/Assets/UI/UIBar.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBar : MonoBehaviour {
+
+    public Image bar;
+
+    public float barTime = 0.1f;
+
+    public float powerLevel = 0.1f;
+    public float amountToAdd = 0.0f;
+
+    public enum PowerUpType
+    {
+        PowerUp,
+        PowerDown
+    }
+
+    public PowerUpType powerUp;
+
+	void OnTriggerEnter () {
+        switch (powerUp)
+        {
+            case PowerUpType.PowerUp:
+                StartCoroutine(PowerUpBar());
+            break;
+
+            case PowerUpType.PowerDown:
+                StartCoroutine(PowerDownBar());
+                break;
+        }
+	}
+	
+    IEnumerator PowerUpBar ()
+    {
+        while (bar.fillAmount < 1)
+        {
+            bar.fillAmount += amountToAdd;
+            yield return new WaitForSeconds(barTime);
+        }
+
+    }
+
+    IEnumerator PowerDownBar()
+    {
+        float tempAmmount = powerLevel;
+        float fillAmmount = bar.fillAmount;
+        while (powerLevel > 0)
+        {
+            fillAmount = tempAmount - amountToAdd;
+            bar.fillAmount -= fillAmount;
+            yield return new WaitForSeconds(barTime);
+        }
+
+    }
+
+}
